8bead2eec01cb4d42d0eb4200db31419326d5d04
PacocaApps <gpaaadm@gmail.com>
2019-01-05 21:12:19 -0200

    Loading APP THAN THE BOT

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var app = express();
+const { execFile } = require('child_process');
 
+
+
+
+const child = execFile('bot/bot.js', ['--version'], (error, stdout, stderr) => {
+  if (error) {
+    throw error;
+  }
+  console.log(stdout);
+});
 
 
 
@@ -13,41 +23,6 @@ app.get('/', function (req, res) {
   });
   
 
-
-
-  //START BOT PROCESS
-
-// function runScript(scriptPath, callback) {
-
-//     // keep track of whether callback has been invoked to prevent multiple invocations
-//     var invoked = false;
-
-//     var process = childProcess.fork(scriptPath);
-
-//     // listen for errors as they may prevent the exit event from firing
-//     process.on('error', function (err) {
-//         if (invoked) return;
-//         invoked = true;
-//         callback(err);
-//     });
-
-//     // execute the callback once the process has finished running
-//     process.on('exit', function (code) {
-//         if (invoked) return;
-//         invoked = true;
-//         var err = code === 0 ? null : new Error('exit code ' + code);
-//         callback(err);
-//     });
-
-// }
-
-// // Now we can run a script and invoke a callback when complete, e.g.
-// runScript('bot/bot.js', function (err) {
-//     if (err) throw err;
-//     console.log('finished running some-script.js');
-// });
-
-//FINISH CALLING THE BOT
 
 
 
