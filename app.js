dd6a028c668504c82339f32c8eb1bd116d0632a5
PacocaApps <gpaaadm@gmail.com>
2019-01-05 20:17:13 -0200

    Bot Functions Added

--- app.js
+++ app.js
@@ -16,6 +16,40 @@ app.get('/', function (req, res) {
 
 
 
+  //START BOT PROCESS
+  var childProcess = require('child_process');
+
+function runScript(scriptPath, callback) {
+
+    // keep track of whether callback has been invoked to prevent multiple invocations
+    var invoked = false;
+
+    var process = childProcess.fork(scriptPath);
+
+    // listen for errors as they may prevent the exit event from firing
+    process.on('error', function (err) {
+        if (invoked) return;
+        invoked = true;
+        callback(err);
+    });
+
+    // execute the callback once the process has finished running
+    process.on('exit', function (code) {
+        if (invoked) return;
+        invoked = true;
+        var err = code === 0 ? null : new Error('exit code ' + code);
+        callback(err);
+    });
+
+}
+
+// Now we can run a script and invoke a callback when complete, e.g.
+runScript('/bot/bot.js', function (err) {
+    if (err) throw err;
+    console.log('finished running some-script.js');
+});
+
+//FINISH CALLING THE BOT
 
 
 
@@ -24,7 +58,35 @@ app.get('/', function (req, res) {
 
 
 
-                                
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
     console.log('Umbler listening on port %s', port);
