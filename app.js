af61046e6ecb8b664fbe525b3096896c22ee3df8
PacocaApps <gpaaadm@gmail.com>
2019-01-05 21:22:20 -0200

    ups

--- app.js
+++ app.js
@@ -3,25 +3,25 @@ var app = express();
 const { execFile } = require('child_process');
 
 
+app.get('/', function (req, res) {
+    res.send('Pacoca Node Server v1.0');
+  });
+  
 
 
-const child = execFile('bot/bot.js', ['--version'], (error, stdout, stderr) => {
-  if (error) {
-    throw error;
-  }
-  console.log(stdout);
-});
+
+const child = execFile('bot/bot.js')//, ['--version'], (error, stdout, stderr) => {
+//   if (error) {
+//     throw error;
+//   }
+//   console.log(stdout);
+// });
 
 
 
 console.log("APP FUCKING LOADED")
 
 
-
-app.get('/', function (req, res) {
-    res.send('Pacoca Node Server v1.0');
-  });
-  
 
 
 
