eebda41c242894e8ecf644d45b6575b21f411395
PacocaApps <gpaaadm@gmail.com>
2019-01-07 16:11:50 -0200

    test2

--- app.js
+++ app.js
@@ -90,6 +90,7 @@ app.listen(port, function () {
 "use strict";
 // Optional. You will see this name in eg. 'ps' or 'top' command
 process.title = 'node-chat';
+var server = app;
 // Port where we'll run the websocket server
 var webSocketsServerPort = 8080 ;
 // websocket and http servers
