851972aa2dd9de9eb9a5fda698088fa4a9419310
PacocaApps <gpaaadm@gmail.com>
2019-01-07 16:20:43 -0200

    tst4

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
 var exec = require('child_process').exec;
-
+var webSocketServer = require('websocket').server;
+var http = require('http');
 
 app.get('/', function (req, res) {
     res.send('Pacoca Node Server v1.21');
@@ -111,12 +112,11 @@ var server = http.createServer(function(request, response) {
 "use strict";
 // Optional. You will see this name in eg. 'ps' or 'top' command
 process.title = 'node-chat';
-var server = app;
+
 // Port where we'll run the websocket server
 
 // websocket and http servers
-var webSocketServer = require('websocket').server;
-var http = require('http');
+
 /**
  * Global variables
  */
