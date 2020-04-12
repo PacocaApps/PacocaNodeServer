8fd8310791f9adb991eaefead5be0e124b9e783d
PacocaApps <gpaaadm@gmail.com>
2020-04-11 21:17:12 -0300

    Update app.js

--- app.js
+++ app.js
@@ -28,9 +28,9 @@ app.get('/products/:id', function (req, res, next) {
   res.json({msg: 'This is CORS-enabled for all origins!'})
 })
 
-// app.listen(80, function () {
-//   console.log('CORS-enabled web server listening on port 80')
-// })
+app.listen(7445, function () {
+  console.log('CORS-enabled web server listening on port 80')
+})
 
 
 
